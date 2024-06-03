2e6d61a1ec8820f0d6a804bb61918e3897df08f6
IgorMedvedev <psydov81@gmail.com>
2024-06-03 17:47:01 +0300

    cw

--- src/telran/util/test/LinkedListTest.java
+++ src/telran/util/test/LinkedListTest.java
@@ -2,6 +2,7 @@ package telran.util.test;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import telran.util.ArrayList;
 import telran.util.LinkedList;
 
 public class LinkedListTest extends ListTest {
